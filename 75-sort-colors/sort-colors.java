b28512a1a059f03bd8b7fee8d444947b3e1e0033
Shubham Patil <92016883+127shubham@users.noreply.github.com>
2024-01-23 23:53:30 +0530

    Update sort-colors.java

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -6,3 +6,25 @@ class Solution {
         
     }
 }
+// using Three-Pointer Approach
+class Solution {
+    public void sortColors(int[] nums) {
+
+        int zero = 0;
+        int two = nums.length;
+        int temp = 0;
+        int n = nums.length;
+
+        while(temp < n && temp <= two){
+            if(nums[temp] == 0){
+                swap(nums, temp++, zero++);
+            }
+            else if(nums[temp] == 2){
+                swap(nums, temp, two--)
+            }
+            else
+            temp++;
+        }
+        
+    }
+}
