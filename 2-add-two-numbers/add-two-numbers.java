e3584fb9fee522e56c2a98ee0178c604a02015c3
Shubham Patil <92016883+127shubham@users.noreply.github.com>
2024-01-31 21:38:42 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (68.42%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -6,7 +6,7 @@
  *     ListNode() {}
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ * }m 
  */
 
 class Solution {
